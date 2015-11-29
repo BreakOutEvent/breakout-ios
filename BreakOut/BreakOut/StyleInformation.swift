6da863cf7a7ca87cdb0b3920c63623ea8629b811
Leo Käßner <leokaessner@me.com>
2015-11-29 23:12:05 +0100

    Adds new Style struct for global styling information like colors and more. The current Data is only placeholder Data.

---
+++ BreakOut/BreakOut/StyleInformation.swift
@@ -0,0 +1,27 @@
+//
+//  StyleInformation.swift
+//  BreakOut
+//
+//  Created by Leo Käßner on 29.11.15.
+//  Copyright © 2015 BreakOut. All rights reserved.
+//
+
+import Foundation
+
+struct Style {
+    
+    //TODO: @alexkaessner -> Update this information.
+    static let mainBackgroundColor: UIColor = UIColor(red:0.17, green:0.18, blue:0.23, alpha:1)
+    static let lightBackgroundColor: UIColor = UIColor(red:0.24, green:0.24, blue:0.3, alpha:1)
+    
+    static let navigationBarGrey: UIColor = UIColor(red:0.57, green:0.58, blue:0.62, alpha:1)
+    
+    static let normalButtonColor: UIColor = UIColor(red:0.77, green:0.29, blue:0.96, alpha:1)
+    static let greyButtonColor: UIColor = UIColor(red:0.57, green:0.58, blue:0.62, alpha:1)
+    static let barButtonColor: UIColor = UIColor(red:0.57, green:0.58, blue:0.62, alpha:1)
+    
+    static let graphLineColor: UIColor = UIColor(red:0.77, green:0.29, blue:0.96, alpha:1)
+    static let graphGradientColor: UIColor = UIColor(red:0.77, green:0.29, blue:0.96, alpha:0.05)
+    
+    static let normalTextColor: UIColor = UIColor.whiteColor()
+}
