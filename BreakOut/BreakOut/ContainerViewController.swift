284692ad78b9d51ce748bf3bf154747ce05e8471
Leo Käßner <leokaessner@me.com>
2016-01-06 09:47:16 +0100

    Adds framework for sidemenu

---
+++ BreakOut/BreakOut/ContainerViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ContainerViewController.swift
+//  BreakOut
+//
+//  Created by Leo Käßner on 03.01.16.
+//  Copyright © 2016 BreakOut. All rights reserved.
+//
+
+import UIKit
+
+import SlideMenuControllerSwift
+
+class ContainerViewController: SlideMenuController {
+    
+    override func awakeFromNib() {
+        if let controller = self.storyboard?.instantiateViewControllerWithIdentifier("AboutBreakOutViewController") {
+            self.mainViewController = controller
+        }
+        if let controller = self.storyboard?.instantiateViewControllerWithIdentifier("UserProfileTableViewController") {
+            self.leftViewController = controller
+        }
+        
+        SlideMenuOptions.contentViewScale = 1.0
+        
+        super.awakeFromNib()
+    }
+
+}
