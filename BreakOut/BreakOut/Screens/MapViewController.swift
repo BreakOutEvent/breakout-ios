99f32d2e2a67eff083ddce3492d554eeaa1a7a9a
dsymhoven <davidsymhoven@David-Symhovens-MacBook-Pro.local>
2016-05-04 21:50:08 +0200

    adds refresh button in MapViewController

--- BreakOut/BreakOut/Screens/MapViewController.swift
+++ BreakOut/BreakOut/Screens/MapViewController.swift
@@ -16,6 +16,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     // > fetch user location from backend
     // > delete dummy user.swift
     // > add sideView in annotations
+    // > set alpha of navigationbar background
     
     let initalLocation = CLLocation(latitude: 48.13842, longitude: 11.57917)
     let regionRadius : CLLocationDistance = 5000
@@ -25,7 +26,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!{
         didSet{
-            mapView.mapType = .Satellite
+            mapView.mapType = .Standard
             mapView.delegate = self
             createUserArray()
             for name in users{
@@ -40,5 +41,40 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         let Florian = User(name: "Florian", locationName: "Dresden", coordinate: CLLocationCoordinate2D(latitude: 51.050409, longitude: 13.737262))
         users.append(David)
         users.append(Florian)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Style the navigation bar
+        self.navigationController!.navigationBar.translucent = true
+        self.navigationController!.navigationBar.barTintColor = Style.mainOrange
+        self.navigationController!.navigationBar.backgroundColor = Style.mainOrange
+        self.navigationController!.navigationBar.tintColor = UIColor.whiteColor()
+        self.navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+
+        self.title = "MapView"
+        
+        // Create save button for navigation item
+        let rightButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Refresh, target: self, action: #selector(fetchLocations))
+        navigationItem.rightBarButtonItem = rightButton
+    }
+    
+    let blc = BasicLocationController()
+    let annotation = MKAnnotation()
+    
+    private func fetchLocations(){
+        print("button pressed")
+        blc.getAllLocations { (locations, error) in
+            if error != nil{
+                self.drawLocationsOnMap(locations!)
+            }
+        }
+            
+        }
+    
+    private func drawLocationsOnMap(location:[BOLocation]){
+        for places in location{
+            mapView.addAnnotation(MKAnnotation)
+        }
     }
 }
